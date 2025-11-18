de1e7c6a5a976aca72fa23f29a83f3b1678286b2
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2025-11-18 21:53:01 +0200

    Solution v2

--- task.sh
+++ task.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-export DB_USER="backup"
-export DB_PASSWORD="<password>"
+DB_HOST="localhost"
+DB_PROD="ShopDB"
+DB_RESERVE="ShopDBReserve"
+DB_DEV="ShopDBDevelopment"
 
 USER="$DB_USER"
 PASSWORD="$DB_PASSWORD"
 
-mysqldump -u"$USER" -p"$PASSWORD" --databases ShopDB > full_backup.sql
-mysql -u"$USER" -p"$PASSWORD" ShopDBReserve < full_backup.sql
+mysqldump -h"$DB_HOST" -u"$USER" -p"$PASSWORD" "$DB_PROD" > full_backup.sql
+mysql -h"$DB_HOST" -u"$USER" -p"$PASSWORD" "$DB_RESERVE" < full_backup.sql
 
-mysqldump -u"$USER" -p"$PASSWORD" --no-create-info ShopDB > backup_data.sql
-mysql -u"$USER" -p"$PASSWORD" ShopDBDevelopment < backup_data.sql
+mysqldump -h"$DB_HOST" -u"$USER" -p"$PASSWORD" --no-create-info "$DB_PROD" > backup_data.sql
+mysql -h"$DB_HOST" -u"$USER" -p"$PASSWORD" "$DB_DEV" < backup_data.sql
